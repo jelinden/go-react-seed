d4cd74a70275e053b780b77e5a60a57f00ac6d1b
Jouni Linden <jouni.linden@gmail.com>
2015-10-31 20:19:13 +0200

    modified the email message a bit

--- app/email/email.go
+++ app/email/email.go
@@ -13,11 +13,10 @@ func SendVerificationEmail(emailTo, hash, fromEmail, passwd string) {
 		"To: " + emailTo + "\n" +
 		"Subject: Please verify your new account\n" +
 		mime +
-		"<div>Please verify your account with following link:<br/>" +
+		"Please verify your account with following link:<br/>" +
 		"<a href=\"" + verficationLink + hash + "\">Verify</a>.<br/><br/>" +
 		"If you received this message without registering to go-react-seed.uutispuro.fi," +
-		" you can delete the message." +
-		"</div>"
+		" you can delete the message."
 
 	sendEmail(emailTo, message, fromEmail, passwd)
 }
